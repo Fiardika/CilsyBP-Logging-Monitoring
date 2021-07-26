35e49899fbd99dfd8c34142d3bbea1a0fe877058
Fiardika <81453470+Fiardika@users.noreply.github.com>
2021-07-26 12:11:48 +0700

    Update install.sh

--- elastic/install.sh
+++ elastic/install.sh
@@ -5,5 +5,6 @@ kubectl apply -f p-elasticsearch-statefulset.yaml
 kubectl apply -f persistance-volume.yaml
 kubectl apply -f p-kibana-dpy.yaml
 kubectl apply -f p-ekf-ingress.yaml
-kubectl apply -f filebeat-kubernetes.yaml
-kubectl apply -f metricbeat-kubernetes.yaml
+kubectl apply -f p-fluentd.yaml
+#kubectl apply -f filebeat-kubernetes.yaml
+#kubectl apply -f metricbeat-kubernetes.yaml
